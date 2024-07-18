0c2804df21ddd270de02356924dcfaab0b3b5e7f
Timur Valiev <cptchunk@yandex.ru>
2024-07-19 00:04:26 +0300

    fib

---
+++ pop/fibonacci_number.py
@@ -0,0 +1,50 @@
+"""
+https://leetcode.com/problems/two-sum/
+
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, 
+such that each number is the sum of the two preceding ones, starting from 0 and 1.
+That is,
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+Example 1:
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+Example 2:
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+Example 3:
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+Constraints:
+0 <= n <= 30
+"""
+
+
+def fib(n: int):
+    if n == 0:
+        return 0
+
+    def _gen():
+        a, b = 1, 1
+        idx = 0
+        for _ in range(n):
+            idx += 1
+            yield a, idx
+            a, b = b, a + b
+    fib_seq = tuple(_gen())
+
+    return fib_seq[-1][0]
+
+if __name__ == '__main__':
+    assert 3 == fib(4)
+    assert 2 == fib(3)
+    assert 1 == fib(1)
+    assert 0 == fib(0)
