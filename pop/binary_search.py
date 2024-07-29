7ebd9d10f03fe9142092bdff55ddc845f40ee943
Timur Valiev <cptchunk@yandex.ru>
2024-07-29 13:00:01 +0300

    binary_search

---
+++ pop/binary_search.py
@@ -0,0 +1,49 @@
+"""
+https://leetcode.com/problems/binary-search/
+
+Given an array of integers nums which is sorted in ascending order,
+and an integer target,
+write a function to search target in nums.
+If target exists, then return its index.
+Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+
+Constraints:
+1 <= nums.length <= 104
+-104 < nums[i], target < 104
+All the integers in nums are unique.
+nums is sorted in ascending order.
+"""
+
+
+def search(nums: list[int], target: int) -> int:
+    as_none = -1
+    low_idx = 0
+    high_idx = len(nums) - 1
+
+    while low_idx <= high_idx:
+        mid_idx = (low_idx + high_idx) // 2
+        if nums[mid_idx] == target:
+            return mid_idx
+        elif nums[mid_idx] < target:
+            low_idx = mid_idx + 1
+        elif nums[mid_idx] > target:
+            high_idx = mid_idx - 1
+
+    return as_none
+
+
+if __name__ == '__main__':
+    assert 4 == search([-1, 0, 3, 5, 9, 12], 9)
+    assert -1 == search([-1, 0, 3, 5, 9, 12], 2)
