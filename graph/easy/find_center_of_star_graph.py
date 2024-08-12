5746f63ba3b87f86f5c97a94183bbb06b1e744be
Timur Valiev <cptchunk@yandex.ru>
2024-08-12 11:04:47 +0300

    find_center_of_star_graph

--- graph/easy/find_center_of_star_graph.py
+++ graph/easy/find_center_of_star_graph.py
@@ -12,7 +12,8 @@ Return the center of the given star graph.
 Example 1:
 Input: edges = [[1,2],[2,3],[4,2]]
 Output: 2
-Explanation: As shown in the figure above, node 2 is connected to every other node, so 2 is the center.
+Explanation: As shown in the figure above,
+node 2 is connected to every other node, so 2 is the center.
 
 Example 2:
 Input: edges = [[1,2],[5,1],[1,3],[1,4]]
@@ -29,7 +30,25 @@ The given edges represent a valid star graph.
 
 
 def find_center(edges: list[list[int]]) -> int:
-    ...
+    table: dict[int, int] = {}
+    maximum: tuple[int, int] = (0, 0)
+
+    for first, second in edges:
+        if not table.get(first):
+            table[first] = 1
+        else:
+            table[first] += 1
+
+        if not table.get(second):
+            table[second] = 1
+        else:
+            table[second] += 1
+
+    for i in table:
+        if table[i] > maximum[1]:
+            maximum = (i, table[i])
+
+    return maximum[0]
 
 
 if __name__ == '__main__':
